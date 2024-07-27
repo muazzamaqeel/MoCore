44f2675f816863ed4035171696bcb599ce716352
muazzamaqeel <aqeemu01@thu.de>
2024-07-27 12:29:32 +0200

    Structure

---
+++ MoCore 1.0/MoCore 1.0/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MoCore_1._0
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
