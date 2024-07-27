c9bd30abf3398196cdd57adf702c17fb90893c69
muazzamaqeel <aqeemu01@thu.de>
2024-07-27 12:44:23 +0200

    Simple Functionality Test

--- MoCore 1.0/MoCore 1.0/MainWindow.xaml.cs
+++ MoCore 1.0/MoCore 1.0/MainWindow.xaml.cs
@@ -1,24 +1,70 @@
-﻿using System.Text;
+﻿using System;
+using System.Diagnostics;
+using System.Management;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace MoCore_1._0
+namespace MoCore_1_0
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializePerformanceCounters();
+            InitializeTimer();
+        }
+
+        private void InitializePerformanceCounters()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdatePerformanceData();
+        }
+
+        private void UpdatePerformanceData()
+        {
+            float cpuUsage = cpuCounter.NextValue();
+            float availableMemory = ramCounter.NextValue();
+            float totalMemory = GetTotalMemoryInMB();
+
+            CpuUsageText.Text = $"CPU Usage: {cpuUsage:F1}%";
+            MemoryUsageText.Text = $"Memory Usage: {totalMemory - availableMemory:F1} MB / {totalMemory:F1} MB";
+        }
+
+        private float GetTotalMemoryInMB()
+        {
+            var query = new ObjectQuery("SELECT TotalPhysicalMemory FROM Win32_ComputerSystem");
+            using (var searcher = new ManagementObjectSearcher(query))
+            {
+                foreach (var item in searcher.Get())
+                {
+                    return Convert.ToSingle(item["TotalPhysicalMemory"]) / (1024 * 1024);
+                }
+            }
+            return 0;
+        }
+
+        private void StartMonitoring_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
