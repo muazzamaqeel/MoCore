c7643705158b56c0771cd84bee45a861a7c8d390
muazzamaqeel <aqeemu01@thu.de>
2024-08-10 13:12:39 +0200

    Strcture

---
+++ MoCore 1.0/MoCore 1.0/ConcertClasses/FileSorting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoCore_1_0.ConcertClasses
+{
+    class FileSorting
+    {
+    }
+}
